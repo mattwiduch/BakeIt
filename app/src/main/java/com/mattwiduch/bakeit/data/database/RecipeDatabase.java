00a92d6c78593bf6c42f67b225a50e1be7f893a6
Mateusz Widuch <matt.widuch@gmail.com>
2017-12-04 17:46:11 +0000

    feat: Improve database singleton creation

--- app/src/main/java/com/mattwiduch/bakeit/data/database/RecipeDatabase.java
+++ app/src/main/java/com/mattwiduch/bakeit/data/database/RecipeDatabase.java
@@ -4,32 +4,40 @@ import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.util.Log;
 import com.mattwiduch.bakeit.data.database.entries.Ingredient;
 import com.mattwiduch.bakeit.data.database.entries.Recipe;
 import com.mattwiduch.bakeit.data.database.entries.Step;
 
 /**
- * Room database layer on top of an SQLite database that stores recipe data..
+ * {@link RecipeDatabase} database for the application including a table for {@link Recipe},
+ * {@link Ingredient} and {@link Step} with appropriate DAOs.
  */
 
+// List of the entry classes
 @Database(entities = {Recipe.class, Ingredient.class, Step.class}, version = 1)
 public abstract class RecipeDatabase extends RoomDatabase {
 
-  private static RecipeDatabase INSTANCE;
+  private static final String LOG_TAG = RecipeDatabase.class.getSimpleName();
+  private static final String DATABASE_NAME = "recipe_database";
+
+  // For singleton instantiation
+  private static final Object LOCK = new Object();
+  private static RecipeDatabase sInstance;
 
   // Create RecipeDatabase singleton to prevent having multiple instances of the database opened
   // at the same time
   public static RecipeDatabase getDatabase(final Context context) {
-    if (INSTANCE == null) {
-      synchronized (RecipeDatabase.class) {
-        if (INSTANCE == null) {
-          INSTANCE = Room.databaseBuilder(context.getApplicationContext(), RecipeDatabase.class,
-              "recipe_database").build();
-        }
+    if (sInstance == null) {
+      synchronized (LOCK) {
+        sInstance = Room.databaseBuilder(context.getApplicationContext(), RecipeDatabase.class,
+              DATABASE_NAME).build();
+        Log.d(LOG_TAG, "Created new database");
       }
     }
-    return INSTANCE;
+    return sInstance;
   }
 
+  // The associated DAOs for the database
   public abstract RecipeDao recipeDao();
 }
