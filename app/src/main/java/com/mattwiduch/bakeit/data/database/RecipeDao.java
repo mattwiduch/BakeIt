9567fa186ba8d5738e98f24544b458b944fe12d6
Mateusz Widuch <matt.widuch@gmail.com>
2017-12-04 17:02:52 +0000

    feat: Add Database Access Object for recipe entities

---
+++ app/src/main/java/com/mattwiduch/bakeit/data/database/RecipeDao.java
@@ -0,0 +1,22 @@
+package com.mattwiduch.bakeit.data.database;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+import java.util.List;
+
+/**
+ * Database access object. Provides queries for inserting and getting recipes.
+ */
+
+@Dao
+public interface RecipeDao {
+
+  @Insert (onConflict = OnConflictStrategy.REPLACE)
+  void bulkInsert(Recipe... recipes);
+
+  @Query("SELECT * FROM recipes ORDER BY name ASC")
+  LiveData<List<Recipe>> getAllRecipes();
+}
