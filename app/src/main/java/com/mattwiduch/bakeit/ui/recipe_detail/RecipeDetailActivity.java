3bf60a5c46e354061676c4814d236351b1ecfe7e
Mateusz Widuch <matt.widuch@gmail.com>
2017-12-07 15:21:36 +0000

    feat: Remove toolbar from recipe detail activity

--- app/src/main/java/com/mattwiduch/bakeit/ui/recipe_detail/RecipeDetailActivity.java
+++ app/src/main/java/com/mattwiduch/bakeit/ui/recipe_detail/RecipeDetailActivity.java
@@ -1,17 +1,13 @@
 package com.mattwiduch.bakeit.ui.recipe_detail;
 
-import static android.support.v4.app.NavUtils.navigateUpFromSameTask;
-
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
-import android.view.MenuItem;
-import android.view.View;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.mattwiduch.bakeit.R;
 import com.mattwiduch.bakeit.ui.recipe_detail.RecipeStepAdapter.RecipeStepAdapterOnItemClickHandler;
 import com.mattwiduch.bakeit.ui.step_detail.StepDetailActivity;
@@ -29,6 +25,9 @@ import com.mattwiduch.bakeit.utils.InjectorUtils;
 public class RecipeDetailActivity extends AppCompatActivity implements
     RecipeStepAdapterOnItemClickHandler {
 
+  @BindView(R.id.steps_recycler_view)
+  RecyclerView stepsRecyclerView;
+
   public static final String RECIPE_ID_EXTRA = "RECIPE_ID_EXTRA";
 
   /**
@@ -43,24 +42,16 @@ public class RecipeDetailActivity extends AppCompatActivity implements
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_recipe_detail);
+    ButterKnife.bind(this);
 
-    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setSupportActionBar(toolbar);
-    toolbar.setTitle(getTitle());
-
-    // Show the Up button in the action bar.
-    ActionBar actionBar = getSupportActionBar();
-    if (actionBar != null) {
-      actionBar.setDisplayHomeAsUpEnabled(true);
-    }
-
+    // The detail container view will be present only in the large-screen layouts
+    // (res/values-w900dp). If this view is present, then the activity should be in two-pane mode.
     if (findViewById(R.id.step_detail_container) != null) {
-      // The detail container view will be present only in the
-      // large-screen layouts (res/values-w900dp).
-      // If this view is present, then the
-      // activity should be in two-pane mode.
       mTwoPane = true;
     }
+
+    // Setup recycler view
+    setupRecyclerView();
 
     // Retrieve recipe id from intent extras
     int recipeId = getIntent().getIntExtra(RECIPE_ID_EXTRA, -1);
@@ -81,34 +72,23 @@ public class RecipeDetailActivity extends AppCompatActivity implements
         mStepsAdapter.updateSteps(steps);
       }
     });
-
-    View recyclerView = findViewById(R.id.step_list);
-    assert recyclerView != null;
-    setupRecyclerView((RecyclerView) recyclerView);
   }
 
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    int id = item.getItemId();
-    if (id == android.R.id.home) {
-      // This ID represents the Home or Up button. In the case of this
-      // activity, the Up button is shown. Use NavUtils to allow users
-      // to navigate up one level in the application structure. For
-      // more details, see the Navigation pattern on Android Design:
-      //
-      // http://developer.android.com/design/patterns/navigation.html#up-vs-back
-      //
-      navigateUpFromSameTask(this);
-      return true;
-    }
-    return super.onOptionsItemSelected(item);
-  }
-
-  private void setupRecyclerView(@NonNull RecyclerView recyclerView) {
+  /**
+   * Prepares recycler view that displays list of recipe steps.
+   */
+  private void setupRecyclerView() {
     mStepsAdapter = new RecipeStepAdapter(this);
-    recyclerView.setAdapter(mStepsAdapter);
+    stepsRecyclerView.setAdapter(mStepsAdapter);
+    stepsRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+    stepsRecyclerView.setHasFixedSize(true);
   }
 
+  /**
+   * Responds to item clicks on recipes in the list.
+   *
+   * @param stepId Id of recipe step that has been clicked
+   */
   @Override
   public void onItemClick(int stepId) {
     if (mTwoPane) {
