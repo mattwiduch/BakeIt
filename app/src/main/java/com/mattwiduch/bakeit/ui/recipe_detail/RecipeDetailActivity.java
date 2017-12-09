3b82163b8ed3b0be76fb1463349f6c6fb2c08f0e
Mateusz Widuch <matt.widuch@gmail.com>
2017-12-09 16:42:49 +0000

    feat: Add custom toolbar to recipe list activity

--- app/src/main/java/com/mattwiduch/bakeit/ui/recipe_detail/RecipeDetailActivity.java
+++ app/src/main/java/com/mattwiduch/bakeit/ui/recipe_detail/RecipeDetailActivity.java
@@ -54,6 +54,10 @@ public class RecipeDetailActivity extends AppCompatActivity implements
     setContentView(R.layout.activity_recipe_detail);
     ButterKnife.bind(this);
 
+    // Allows to draw behind status bar
+    getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+
     // The detail container view will be present only in the large-screen layouts
     // (res/values-w600dp). If this view is present, then the activity should be in two-pane mode.
     if (findViewById(R.id.step_detail_container) != null) {
