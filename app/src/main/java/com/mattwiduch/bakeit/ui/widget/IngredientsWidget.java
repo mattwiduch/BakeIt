ab5cd374ef3980295f9827a42c9e234261117299
Mateusz Widuch <matt.widuch@gmail.com>
2018-01-03 14:49:20 +0000

    feat: Launch RecipeDetailActivity when widget is clicked

--- app/src/main/java/com/mattwiduch/bakeit/ui/widget/IngredientsWidget.java
+++ app/src/main/java/com/mattwiduch/bakeit/ui/widget/IngredientsWidget.java
@@ -1,10 +1,13 @@
 package com.mattwiduch.bakeit.ui.widget;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.content.Intent;
 import android.widget.RemoteViews;
 import com.mattwiduch.bakeit.R;
+import com.mattwiduch.bakeit.ui.recipe_detail.RecipeDetailActivity;
 
 /**
  * Implementation of App Widget functionality.
@@ -18,6 +21,13 @@ public class IngredientsWidget extends AppWidgetProvider {
     // Construct the RemoteViews object
     RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.ingredients_widget);
     views.setTextViewText(R.id.appwidget_text, widgetText);
+
+    // Create an Intent to launch RecipeDetailActivity for given recipe when clicked
+    Intent intent = new Intent(context, RecipeDetailActivity.class);
+    // TODO: Change to actual id
+    intent.putExtra(RecipeDetailActivity.RECIPE_ID_EXTRA, 1);
+    PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+    views.setOnClickPendingIntent(R.id.ingredients_widget_container, pendingIntent);
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views);
