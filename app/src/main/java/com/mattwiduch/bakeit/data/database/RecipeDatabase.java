769d3e7b56101813bf33b7faed9d987045759788
Mateusz Widuch <matt.widuch@gmail.com>
2018-01-11 16:56:35 +0000

    feat: Remove redundant singleton instantiation code from RecipeDatabase

--- app/src/main/java/com/mattwiduch/bakeit/data/database/RecipeDatabase.java
+++ app/src/main/java/com/mattwiduch/bakeit/data/database/RecipeDatabase.java
@@ -1,10 +1,7 @@
 package com.mattwiduch.bakeit.data.database;
 
 import android.arch.persistence.room.Database;
-import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
-import android.content.Context;
-import android.util.Log;
 import com.mattwiduch.bakeit.data.database.entries.Ingredient;
 import com.mattwiduch.bakeit.data.database.entries.Recipe;
 import com.mattwiduch.bakeit.data.database.entries.Step;
@@ -18,25 +15,7 @@ import com.mattwiduch.bakeit.data.database.entries.Step;
 @Database(entities = {Recipe.class, Ingredient.class, Step.class}, version = 1, exportSchema = false)
 public abstract class RecipeDatabase extends RoomDatabase {
 
-  private static final String LOG_TAG = RecipeDatabase.class.getSimpleName();
   public static final String DATABASE_NAME = "recipe_database";
-
-  // For singleton instantiation
-  private static final Object LOCK = new Object();
-  private static RecipeDatabase sInstance;
-
-  // Create RecipeDatabase singleton to prevent having multiple instances of the database opened
-  // at the same time
-  public static RecipeDatabase getInstance(final Context context) {
-    if (sInstance == null) {
-      synchronized (LOCK) {
-        sInstance = Room.databaseBuilder(context.getApplicationContext(), RecipeDatabase.class,
-              DATABASE_NAME).build();
-        Log.d(LOG_TAG, "Created new database");
-      }
-    }
-    return sInstance;
-  }
 
   // The associated DAOs for the database
   public abstract RecipeDao recipeDao();
