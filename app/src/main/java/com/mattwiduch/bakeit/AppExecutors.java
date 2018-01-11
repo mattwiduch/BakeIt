4b5768096e5fcad7a76c539bae0cb8239bf1c1b2
Mateusz Widuch <matt.widuch@gmail.com>
2018-01-11 19:09:41 +0000

    feat: Make AppExecutors injectable

--- app/src/main/java/com/mattwiduch/bakeit/AppExecutors.java
+++ app/src/main/java/com/mattwiduch/bakeit/AppExecutors.java
@@ -33,15 +33,12 @@ import javax.inject.Singleton;
 @Singleton
 public class AppExecutors {
 
-  // For Singleton instantiation
-  private static final Object LOCK = new Object();
-  private static AppExecutors sInstance;
   private final Executor diskIO;
   private final Executor mainThread;
   private final Executor networkIO;
 
   @Inject
-  public AppExecutors() {
+  AppExecutors() {
     this(Executors.newSingleThreadExecutor(), Executors.newFixedThreadPool(3),
         new MainThreadExecutor());
   }
@@ -50,17 +47,6 @@ public class AppExecutors {
     this.diskIO = diskIO;
     this.networkIO = networkIO;
     this.mainThread = mainThread;
-  }
-
-  public static AppExecutors getInstance() {
-    if (sInstance == null) {
-      synchronized (LOCK) {
-        sInstance = new AppExecutors(Executors.newSingleThreadExecutor(),
-            Executors.newFixedThreadPool(3),
-            new MainThreadExecutor());
-      }
-    }
-    return sInstance;
   }
 
   public Executor diskIO() {
