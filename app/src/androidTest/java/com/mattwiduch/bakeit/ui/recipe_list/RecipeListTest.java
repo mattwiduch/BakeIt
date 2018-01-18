68918195a3cdc9483b0b645e29dcfbd0e7e67aa8
Mateusz Widuch <matt.widuch@gmail.com>
2018-01-18 14:59:24 +0000

    test: Setup RecipeListFragment testing environment

--- app/src/androidTest/java/com/mattwiduch/bakeit/ui/recipe_list/RecipeListTest.java
+++ app/src/androidTest/java/com/mattwiduch/bakeit/ui/recipe_list/RecipeListTest.java
@@ -1,31 +1,20 @@
 package com.mattwiduch.bakeit.ui.recipe_list;
 
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
-import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import android.arch.lifecycle.MutableLiveData;
 import android.content.Context;
-import android.support.annotation.NonNull;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import com.mattwiduch.bakeit.R;
 import com.mattwiduch.bakeit.data.database.entries.Recipe;
 import com.mattwiduch.bakeit.data.network.RecipeNetworkDataSource;
 import com.mattwiduch.bakeit.testing.SingleFragmentActivity;
 import com.mattwiduch.bakeit.util.EspressoTestUtil;
-import com.mattwiduch.bakeit.util.RecyclerViewMatcher;
 import com.mattwiduch.bakeit.util.TaskExecutorWithIdlingResourceRule;
 import com.mattwiduch.bakeit.util.ViewModelUtil;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Rule;
@@ -34,5 +23,37 @@ import org.junit.runner.RunWith;
 
 @RunWith(AndroidJUnit4.class)
 public class RecipeListTest {
+  @Rule
+  public ActivityTestRule<SingleFragmentActivity> activityTestRule =
+      new ActivityTestRule<>(SingleFragmentActivity.class, true, true);
 
+  @Rule
+  public TaskExecutorWithIdlingResourceRule executorTestRule =
+      new TaskExecutorWithIdlingResourceRule();
+
+  private MutableLiveData<List<Recipe>> recipes = new MutableLiveData<>();
+  private RecipeListFragment recipeListFragment;
+  private RecipeListViewModel viewModel;
+  private RecipeNetworkDataSource dataSource;
+
+  @Before
+  public void init() {
+    EspressoTestUtil.disableProgressBarAnimations(activityTestRule);
+    recipeListFragment = new RecipeListFragment();
+    viewModel = mock(RecipeListViewModel.class);
+    dataSource = mock(RecipeNetworkDataSource.class);
+    when(viewModel.getAllRecipes()).thenReturn(recipes);
+
+    recipeListFragment.viewModelFactory = ViewModelUtil.createFor(viewModel);
+    recipeListFragment.recipeNetworkDataSource = dataSource;
+    activityTestRule.getActivity().setFragment(recipeListFragment);
+  }
+
+  @Test
+  public void useAppContext() throws Exception {
+    // Context of the app under test.
+    Context appContext = InstrumentationRegistry.getTargetContext();
+
+    assertEquals("com.mattwiduch.bakeit", appContext.getPackageName());
+  }
 }
